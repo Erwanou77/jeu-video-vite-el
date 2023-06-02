314ce2bef2121fb91348d9095b6756d07a1ba2bd
Erwanou77 <erwan.launay77@gmail.com>
2023-06-02 09:20:20 +0200

    Init du projet

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,10 @@
+const NotFound = () => {
+    return (
+        <div>
+            La page demandé n'a pas été trouvé 
+            <strong>Erreur 404</strong>
+        </div>
+    );
+};
+
+export default NotFound;

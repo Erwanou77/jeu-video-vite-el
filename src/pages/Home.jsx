6bb9f492de372dbe3fd9476b3f74a4a0921c827b
Erwanou77 <erwan.launay77@gmail.com>
2023-06-05 03:48:51 +0200

    📦 NEW: Page d'accueil avec ApiCall + cards

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,29 @@
+import { useState } from "react";
+import ApiCall from "../components/ApiCall";
+import Cards from "../components/Cards";
+
 const Home = () => {
+    const [page, setPage] = useState(1);
+    const handleLoadMore = () => {
+        setPage(prevCount => prevCount + 1);
+    };
+    const params = {
+        key: '703ab7abdb434e81b8492cd1c95664d6', // Remplacez 'YOUR_API_KEY' par votre clé d'API de RAWG
+        ordering: 'name',
+        page_size: 20,
+        page: page,
+    };
     return (
-        <div>
-            <h1>Test</h1>
-        </div>
+        <section id="home">
+            <div className="galleryCards">
+                <ApiCall
+                    url={`https://api.rawg.io/api/games`}
+                    params={params}
+                    boolList={true}
+                    render={games => Cards(games, handleLoadMore)}
+                />
+            </div>
+        </section>
     );
 };
 
